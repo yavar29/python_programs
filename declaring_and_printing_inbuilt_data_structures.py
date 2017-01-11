dee70ee90532723eee4711366a09a598eb87019a
yavar29 <yavarkhan1997@gmail.com>
2017-01-12 01:38:48 +0530

    added a program to test and learn inbuilt data structures

---
+++ declaring_and_printing_inbuilt_data_structures.py
@@ -0,0 +1,45 @@
+# first method of declaring a list
+l=[]
+l.append("saim")
+l.append("yavar")
+l.append("sidra")
+l.append(101)
+l.append(101.99)
+l.append("adil")
+print(l)
+
+# second method of directly declaring a list
+l1=['saim', 'yavar', 'sidra1', 1013, 101.99, 'adil1']
+print(l1)
+print(type(l1))
+
+# declaring a tuple
+tup=('hello','bye',33,77.8)
+print(tup)
+print(type(tup))
+
+# first method of declaring a dictionary
+dic={'a':'apple','b':'ball','c':'cat','d':'dog'}
+print(dic)
+print(type(dic))
+
+# second method of declaring a dictionary
+dic1={}
+dic1['sehwag']=101
+dic1['sachin']=199
+dic1['dhoni']=51
+dic1['ashvin']=6
+print(dic1)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ printing_variables.py
@@ -0,0 +1,11 @@
+mystring="hello"
+myfloat=10.0
+myint=20
+
+if mystring=="hello":
+    print('string is'+ ' ' +mystring+"\n")
+if myfloat==10.0:
+    print('float is ' + str(myfloat))
+if myint==20:
+    print('integer is ' + str(myint))
+    
